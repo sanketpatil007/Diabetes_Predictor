9954e00d3f130b4cbbb60889741fce7ae152699d
sanketpatil007 <76589010+sanketpatil007@users.noreply.github.com>
2023-05-04 17:41:22 +0530

    Add files via upload
    django files

---
+++ DiabetesPrediction/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+def home(request):
+    return render(request, 'home.html')
+def predict(request):
+    return render(request, 'predict.html')
+def result(request):
+    df = pd.read_csv(r'D:\Onkar\Diabetes_Pred\diabetes.csv')
+
+    X = df.drop('Outcome', axis=1)
+    Y = df['Outcome']
+    X.replace(to_replace=0, value=X.mean(), inplace=True)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=None)
+    lr = LogisticRegression()
+    lr.fit(X_train, Y_train)
+
+    val1 = float(request.GET.get('pregnancies'))
+    val2 = float(request.GET.get('glucose'))
+    val3 = float(request.GET.get('bloodpressure'))
+    val4 = float(request.GET.get('skinthickness'))
+    val5 = float(request.GET.get('insulin'))
+    val6 = float(request.GET.get('bmi'))
+    val7 = float(request.GET.get('dpf'))
+    val8 = float(request.GET.get('age'))
+
+    pred = lr.predict([[val1, val2, val3, val4, val5, val6, val7, val8]])
+    result1 = ""
+    if pred == [1]:
+        result1 = "Positive"
+    else:
+        result1 ="Negative"
+    return render(request, 'predict.html', {"result2":result1})
